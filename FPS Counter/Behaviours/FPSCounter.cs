f25db0b7c9055cf699ce3aba25f351774066ee73
Eris <erisapps@outlook.be>
2020-07-26 03:22:22 +0200

    Made FPSCounter Init logic more readable
    Fixed naming as well

--- FPS Counter/Behaviours/FPSCounter.cs
+++ FPS Counter/Behaviours/FPSCounter.cs
@@ -9,152 +9,152 @@ using UnityEngine.XR;
 
 namespace FPS_Counter.Behaviours
 {
-    internal class FPSCounter : MonoBehaviour
-    {
-        private TMP_Text counter;
-        private GameObject percent;
-        private int targetFramerate;
-        private Image image;
+	internal class FpsCounter : MonoBehaviour
+	{
+		private TMP_Text _counter;
+		private GameObject _percent;
+		private int _targetFramerate;
+		private Image _image;
 
-        private float numFrames;
-        private float lastFrameTime;
-        private float nextCounterUpdate = Time.time + Configuration.Instance.UpdateRate;
-        private float ringFillPercent = 1;
+		private float _numFrames;
+		private float _lastFrameTime;
+		private float _nextCounterUpdate = Time.time + Configuration.Instance.UpdateRate;
+		private float _ringFillPercent = 1;
 
-        private void Awake()
-        {
-            try
-            {
-                Logger.Log.Debug("Attempting to Initialize FPS Counter");
-                Init();
-            }
-            catch (Exception ex)
-            {
-                Logger.Log.Error("FPS Counter Done screwed up on initialization"); // -Kyle1413
-                Logger.Log.Error(ex);
-            }
-        }
+		private void Awake()
+		{
+			try
+			{
+				Logger.Log.Debug("Attempting to Initialize FPS Counter");
+				Init();
+			}
+			catch (Exception ex)
+			{
+				Logger.Log.Error("FPS Counter Done screwed up on initialization"); // -Kyle1413
+				Logger.Log.Error(ex);
+			}
+		}
 
-        private void Init()
-        {
-            targetFramerate = (int)XRDevice.refreshRate;
+		private void Init()
+		{
+			_targetFramerate = (int) XRDevice.refreshRate;
 
-            Logger.Log.Info($"Target framerate = {targetFramerate}");
+			Logger.Log.Info($"Target framerate = {_targetFramerate}");
 
-            Canvas canvas = gameObject.AddComponent<Canvas>();
-            canvas.renderMode = RenderMode.WorldSpace;
-            //canvas.transform.Translate(new Vector3(0, 0, -100));
+			Canvas canvas = gameObject.AddComponent<Canvas>();
+			canvas.renderMode = RenderMode.WorldSpace;
 
-            CanvasScaler canvasScaler = gameObject.AddComponent<CanvasScaler>();
-            canvasScaler.scaleFactor = 10.0f;
-            canvasScaler.dynamicPixelsPerUnit = 10f;
+			CanvasScaler canvasScaler = gameObject.AddComponent<CanvasScaler>();
+			canvasScaler.scaleFactor = 10.0f;
+			canvasScaler.dynamicPixelsPerUnit = 10f;
 
-            GraphicRaycaster graphicRaycaster = gameObject.AddComponent<GraphicRaycaster>();
-            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
-            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
+			gameObject.AddComponent<GraphicRaycaster>();
+			gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
+			gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
 
-            Image image = null!;
-            try
-            {
-                ScoreMultiplierUIController scoreMultiplier = Resources.FindObjectsOfTypeAll<ScoreMultiplierUIController>().First();
-                image = BS_Utils.Utilities.ReflectionUtil.GetPrivateField<Image>(scoreMultiplier, "_multiplierProgressImage");
-            }
-            catch (Exception ex)
-            {
-                Logger.Log.Error("oops");
-                Logger.Log.Error(ex);
-            }
+			TextHelper.CreateText(out _counter, canvas, Vector2.zero);
+			_counter.alignment = TextAlignmentOptions.Center;
+			_counter.transform.localScale *= Plugin.IsCountersPlusPresent ? 1 : 0.12f;
+			_counter.fontSize = 2.5f;
+			_counter.color = Color.white;
+			_counter.lineSpacing = -50f;
+			_counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
+			_counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
+			_counter.enableWordWrapping = false;
+			_counter.transform.localPosition = Plugin.IsCountersPlusPresent ? Vector3.zero : new Vector3(-0.1f, 3.5f, 8f);
 
-            TextHelper.CreateText(out counter, canvas, Vector2.zero);
-            counter.alignment = TextAlignmentOptions.Center;
-            counter.transform.localScale *= Plugin.IsCountersPlusPresent ? 1 : 0.12f;
-            counter.fontSize = 2.5f;
-            counter.color = Color.white;
-            counter.lineSpacing = -50f;
-            counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
-            counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
-            counter.enableWordWrapping = false;
-            counter.transform.localPosition = Plugin.IsCountersPlusPresent ? Vector3.zero : new Vector3(-0.1f, 3.5f, 8f);
+			if (!Configuration.Instance.ShowRing)
+			{
+				return;
+			}
 
-            if (Configuration.Instance.ShowRing)
-            {
-                percent = new GameObject();
-                this.image = percent.AddComponent<Image>();
-                percent.transform.SetParent(counter.transform);
-                percent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 2f);
-                percent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 2f);
-                percent.transform.localScale = new Vector3(4f, 4f, 4f);
-                percent.transform.localPosition = Vector3.zero;
+			_percent = new GameObject();
+			_image = _percent.AddComponent<Image>();
+			_percent.transform.SetParent(_counter.transform, false);
+			_percent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 2f);
+			_percent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 2f);
+			_percent.transform.localScale = new Vector3(4f, 4f, 4f);
+			_percent.transform.localPosition = Vector3.zero;
 
-                if (this.image)
-                {
-                    this.image.sprite = image?.sprite;
-                    this.image.type = Image.Type.Filled;
-                    this.image.fillMethod = Image.FillMethod.Radial360;
-                    this.image.fillOrigin = (int)Image.Origin360.Top;
-                    this.image.fillClockwise = true;
-                }
-            }
-        }
+			try
+			{
+				ScoreMultiplierUIController scoreMultiplier = Resources.FindObjectsOfTypeAll<ScoreMultiplierUIController>().First();
+				var multiplierImage = BS_Utils.Utilities.ReflectionUtil.GetPrivateField<Image>(scoreMultiplier, "_multiplierProgressImage");
 
-        private void Update()
-        {
-            if (Time.time > nextCounterUpdate)
-            {
-                float fps = Mathf.Round(numFrames / (Time.time - lastFrameTime));
-                counter.text = $"FPS\n{fps}";
-                ringFillPercent = fps / targetFramerate;
+				if (scoreMultiplier && _image)
+				{
+					_image.sprite = multiplierImage.sprite;
+					_image.type = Image.Type.Filled;
+					_image.fillMethod = Image.FillMethod.Radial360;
+					_image.fillOrigin = (int) Image.Origin360.Top;
+					_image.fillClockwise = true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Log.Error("oops");
+				Logger.Log.Error(ex);
+			}
+		}
 
-                if (Configuration.Instance.UseColors)
-                {
-                    Color color;
+		private void Update()
+		{
+			if (Time.time > _nextCounterUpdate)
+			{
+				float fps = Mathf.Round(_numFrames / (Time.time - _lastFrameTime));
+				_counter.text = $"FPS\n{fps}";
+				_ringFillPercent = fps / _targetFramerate;
 
-                    if (ringFillPercent > 0.95f)
-                    {
-                        color = Color.green;
-                    }
-                    else if (ringFillPercent > 0.7f)
-                    {
-                        color = Color.yellow;
-                    }
-                    else if (ringFillPercent > 0.5f)
-                    {
-                        color = new Color(1, 0.64f, 0);
-                    }
-                    else
-                    {
-                        color = Color.red;
-                    }
+				if (Configuration.Instance.UseColors)
+				{
+					Color color;
 
-                    image?.SetColor(color);
-                    counter.color = color;
-                }
+					if (_ringFillPercent > 0.95f)
+					{
+						color = Color.green;
+					}
+					else if (_ringFillPercent > 0.7f)
+					{
+						color = Color.yellow;
+					}
+					else if (_ringFillPercent > 0.5f)
+					{
+						color = new Color(1, 0.64f, 0);
+					}
+					else
+					{
+						color = Color.red;
+					}
 
-                lastFrameTime = nextCounterUpdate;
-                nextCounterUpdate += Configuration.Instance.UpdateRate;
-                numFrames = 0;
-            }
+					_image?.SetColor(color);
+					_counter.color = color;
+				}
 
-            if (Configuration.Instance.ShowRing)
-            {
-                if (image)
-                {
-                    image.fillAmount = Mathf.Lerp(image.fillAmount, ringFillPercent, 2 * Time.deltaTime);
-                }
-            }
+				_lastFrameTime = _nextCounterUpdate;
+				_nextCounterUpdate += Configuration.Instance.UpdateRate;
+				_numFrames = 0;
+			}
 
-            numFrames++;
-        }
-    }
+			if (Configuration.Instance.ShowRing)
+			{
+				if (_image)
+				{
+					_image.fillAmount = Mathf.Lerp(_image.fillAmount, _ringFillPercent, 2 * Time.deltaTime);
+				}
+			}
 
-    internal static class ExtensionMethods
-    {
-        public static void SetColor(this Image image, Color color)
-        {
-            if (image)
-            {
-                image.color = color;
-            }
-        }
-    }
+			_numFrames++;
+		}
+	}
+
+	internal static class ExtensionMethods
+	{
+		public static void SetColor(this Image image, Color color)
+		{
+			if (image)
+			{
+				image.color = color;
+			}
+		}
+	}
 }
