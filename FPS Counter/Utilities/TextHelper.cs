30187a5f9e43bd4b78f3bb807883c6c0fbce19ab
Eris <erisapps@outlook.be>
2020-07-26 03:14:30 +0200

    Fixed warnings in TextHelper class

--- FPS Counter/Utilities/TextHelper.cs
+++ FPS Counter/Utilities/TextHelper.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 namespace FPS_Counter.Utilities
 {
-    public class TextHelper
-    {
-        // Joink (from Counters+ (since its method was internal (which means inaccessible (which should probably be changed))))
-        public static void CreateText(out TMP_Text tmp_text, Canvas canvas, Vector3 anchoredPosition)
-        {
-            float scale = 10f;
-            RectTransform rectTransform = canvas.transform as RectTransform;
-            rectTransform.sizeDelta = new Vector2(100, 50);
+	internal static class TextHelper
+	{
+		private const float Scale = 10f;
 
-            tmp_text = BeatSaberUI.CreateText(rectTransform, string.Empty, anchoredPosition * scale);
-            tmp_text.alignment = TextAlignmentOptions.Center;
-            tmp_text.fontSize = 4f;
-            tmp_text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 2f);
-            tmp_text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 2f);
-            tmp_text.enableWordWrapping = false;
-            tmp_text.overflowMode = TextOverflowModes.Overflow;
-        }
-    }
+		// Joinked from Counters+ (since its method was internal (which means inaccessible (which should probably be changed)))
+		public static void CreateText(out TMP_Text tmpText, Canvas canvas, Vector3 anchoredPosition)
+		{
+			var rectTransform = canvas.transform as RectTransform;
+			rectTransform!.sizeDelta = new Vector2(100, 50);
+
+			tmpText = BeatSaberUI.CreateText(rectTransform, string.Empty, anchoredPosition * Scale);
+			tmpText.alignment = TextAlignmentOptions.Center;
+			tmpText.fontSize = 4f;
+			tmpText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 2f);
+			tmpText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 2f);
+			tmpText.enableWordWrapping = false;
+			tmpText.overflowMode = TextOverflowModes.Overflow;
+		}
+	}
 }
