d27a9bcc611438de332a237b2dd48b494816f5d0
Eris <erisapps@outlook.be>
2020-09-20 02:50:34 +0200

    Added FPS target percentage valueconverter

---
+++ FPS Counter/Converters/FpsTargetPercentageColorValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace FPS_Counter.Converters
+{
+	internal static class FpsTargetPercentageColorValueConverter
+	{
+		// Allocate beforehand, because invoking a color "constant" will create a new struct every time
+		private static readonly Color Green = Color.green;
+		private static readonly Color Yellow = Color.yellow;
+		private static readonly Color Orange = new Color(1, 0.64f, 0);
+		private static readonly Color Red = Color.red;
+
+		public static Color Convert(float fpsTargetPercentage)
+		{
+			if (fpsTargetPercentage > 0.95f)
+			{
+				return Green;
+			}
+
+			if (fpsTargetPercentage > 0.7f)
+			{
+				return Yellow;
+			}
+
+			if (fpsTargetPercentage > 0.5f)
+			{
+				return Orange;
+			}
+
+			return Red;
+		}
+	}
+}
